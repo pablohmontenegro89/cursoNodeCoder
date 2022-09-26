e9371c412a8d10b58f24a2e0cdd464e589813d1f
pablohmontenegro89 <pablohmontenegro8@gmail.com>
2022-09-26 20:35:28 -0300

    desafioClase12

--- entregable hbs/server.js
+++ entregable hbs/server.js
@@ -1,11 +1,12 @@
 const handlebars = require("express-handlebars");
 const express = require("express");
 const Productos = require("./api/productos.js");
-
+const { Server: IOServer } = require("socket.io");
+const { Server: HttpServer } = require("http");
 let productos = new Productos();
-
 const app = express();
-
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 //--------------------------------------------
 //establecemos la configuración de handlebars
 app.engine(
@@ -19,44 +20,56 @@ app.set("view engine", "hbs");
 app.set("views", "./views");
 //--------------------------------------------
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
+app.get("/", (req, res) => {
+	res.sendFile("index.html");
+});
 
 const router = express.Router();
-app.use("/api", router);
+app.use("/", router);
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.get("/productos/listar", (req, res) => {
-	res.json(productos.listarAll());
-});
+//productos.listarAll()
+//productos.guardar(producto);
 
-router.get("/productos/listar/:id", (req, res) => {
-	let { id } = req.params;
-	res.json(productos.listar(id));
-});
 
-router.post("/productos/guardar", (req, res) => {
-	let producto = req.body;
-	productos.guardar(producto);
-	//res.json(producto)
-	res.redirect("/");
-});
 
-router.put("/productos/actualizar/:id", (req, res) => {
-	let { id } = req.params;
-	let producto = req.body;
-	productos.actualizar(producto, id);
-	res.json(producto);
-});
+const demo = [
+	{
+		nombre: "Escoba",
+		precio: 1000,
+		thumbnail: 'https://github.com/pablohmontenegro89/cursoNodeCoder/commit/d09fd139f67418b71bcaacaeab776592ee59cb6e',
+	},
+	{
+		nombre: "Microondas",
+		precio: 2000,
+		thumbnail: 'https://github.com/pablohmontenegro89/cursoNodeCoder/commit/d09fd139f67418b71bcaacaeab776592ee59cb6e',
+	},
+	{
+		nombre: "Heladera",
+		precio: 3000,
+		thumbnail: 'https://github.com/pablohmontenegro89/cursoNodeCoder/commit/d09fd139f67418b71bcaacaeab776592ee59cb6e',
+	},
+];
 
-router.delete("/productos/borrar/:id", (req, res) => {
-	let { id } = req.params;
-	let producto = productos.borrar(id);
-	res.json(producto);
-});
+const messages = [
+	{
+		author: "Juan",
+		text: "Hola que tal",
+	},
+	{
+		author: "Maria",
+		text: "Bien y vos?",
+	},
+	{
+		author: "Juan",
+		text: "Me alegra",
+	},
+];
 
-router.get("/productos/vista", (req, res) => {
+router.get("/", (req, res) => {
 	let prods = productos.listarAll();
 
 	res.render("vista", {
@@ -65,9 +78,25 @@ router.get("/productos/vista", (req, res) => {
 	});
 });
 
-const PORT = 8080;
 
-const server = app.listen(PORT, () => {
-	console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+io.on("connection", (socket) => {
+	console.log("se cargó un producto");
+
+	socket.emit("demo", demo);
+
+	socket.on("new-product", (data) => {
+		demo.push(data);
+		io.sockets.emit("demo", demo);
+	});
+
+	socket.emit("messages", messages);
+	
+	socket.on("new-message", (data) => {
+		messages.push(data);
+		io.sockets.emit("messages", messages);
+	});
 });
-server.on("error", (error) => console.log(`Error en servidor ${error}`));
+
+
+const PORT = 8080;
+httpServer.listen(PORT, () => console.log("servidor Levantado"));
