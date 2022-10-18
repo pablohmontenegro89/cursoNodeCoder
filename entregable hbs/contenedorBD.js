041aee86336ae0929f4d41592bb1869c5d27aa4a
pablohmontenegro89 <pablohmontenegro8@gmail.com>
2022-10-17 23:11:47 -0300

    Desafio 16

---
+++ entregable hbs/contenedorBD.js
@@ -0,0 +1,26 @@
+const {options} = require("./mariaDB")
+class ContenedorBd {
+    constructor(tabla, config) {
+      this.knex = require("knex")(options);
+      this.tabla = tabla;
+    }
+  
+    async save(objetoAGuardar) {
+      try {
+        return await this.knex(this.tabla).insert(objetoAGuardar);
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  
+    async getAll() {
+      try {
+        let datosAlmacenados = await this.knex.select("*").from(this.tabla);
+        return datosAlmacenados ?? null;
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  }
+  
+  module.exports = { ContenedorBd };
